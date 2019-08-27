3e598ea481f8d8a26c444a4e5589dee0a8642240
Ulugbek <kayrat.nazov@gmail.com>
2019-08-27 12:52:01 +0500

    Filtering by country and specialty to vacancies_list.html is added

--- core/views/vacancies_views.py
+++ core/views/vacancies_views.py
@@ -3,11 +3,35 @@ from vacancy.models import Vacancy
 from django.template.defaulttags import register
 from django.utils.translation import get_language as _
 from bs4 import BeautifulSoup
+from django.db.models import Q
 
 
 class VacanciesListPage(ListView):
     template_name = 'vacancies/vacancies_list.html'
     context_object_name = 'vacancies'
+    paginate_by = 10
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        data = self.request.GET
+        location, specialty = data.get("location"), data.get("specialty")
+        qs = Vacancy.objects.all()
+        if location:
+            qs = qs.filter(
+                Q(location_ru__icontains=location) |
+                Q(location_en__icontains=location) |
+                Q(location_uz__icontains=location) |
+                Q(location_kz__icontains=location)
+            )
+        if specialty:
+            qs = qs.filter(
+                Q(title_en__icontains=specialty) |
+                Q(title_ru__icontains=specialty) |
+                Q(title_uz__icontains=specialty) |
+                Q(title_kz__icontains=specialty)
+            )
+        context["vacancies"] = qs
+        return context
 
     @register.filter
     def no_title_filter(value):
